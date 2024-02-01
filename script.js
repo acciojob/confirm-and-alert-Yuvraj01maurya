142ce7b301ee5b7746f16e89e6ddb2523586a093
Yuvraj Maurya <yuvraj95166@gmail.com>
2024-02-01 23:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Function to ask for confirmation
+function askForConfirmation() {
+    // Display confirmation dialog
+    var userConfirmation = confirm("Do you want to proceed?");
+
+    // Check user's response
+    if (userConfirmation) {
+        alert("You clicked OK. Proceeding...");
+    } else {
+        alert("You clicked Cancel. Exiting...");
+    }
+}
+
+// Call the function to initiate the confirmation process
+askForConfirmation();
+
